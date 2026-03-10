066b97616922a2c5f07a35098ffeff1c0bf0778c
Ankur Kejriwal <ankurkejriwal@Ankurs-MacBook-Pro-2.local>
2026-03-10 18:23:28 -0400

    embodied-data style pyproject

--- setup.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -e ".[dev]"
-pip install build
-echo "Setup complete! Activate with: source .venv/bin/activate"
